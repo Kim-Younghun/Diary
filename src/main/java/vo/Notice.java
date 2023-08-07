78c87ec745bbb62aa6e5e41399df87c5caec79ae
Kim-Younghun <floriaberg59@gmail.com>
2023-08-07 19:17:37 +0900

    [김영훈] vo 누락파일 추가

---
+++ src/main/java/vo/Notice.java
@@ -0,0 +1,11 @@
+package vo;
+
+public class Notice {
+	public int noticeNo;
+	public String noticeTitle;
+	public String noticeContent;
+	public String noticeWriter;
+	public String noticePw;
+	public String createdate;
+	public String updatedate;
+}
